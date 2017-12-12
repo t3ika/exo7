74e9c1ce1b10784b7e02e9f905cb3798c7f9c7f6
t3ika <cedric.koch@outlook.com>
2017-12-12 17:02:43 +0100

    0

--- exo7/Form1.Designer.cs
+++ exo7/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(404, 174);
+            this.button1.Location = new System.Drawing.Point(366, 231);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(433, 132);
+            this.button2.Location = new System.Drawing.Point(447, 231);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
